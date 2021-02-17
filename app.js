9a5df3fef73abcfd7459bbe6d162c41a41de6080
Lin <mt5718214@gmail.com>
2021-02-17 17:17:31 +0800

    install express-session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bodyParser = require('body-parser')
+const session = require('express-session')
 const routes = require('./routes')
 
 const app = express()
@@ -9,6 +10,13 @@ const PORT = process.env.PORT || 3000
 
 app.set('view engine', 'hbs')
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs', helpers: require('./config/handlebars-helpers') }))
+
+app.use(session({
+  secret: 'side project',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 80000 }
+}))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
